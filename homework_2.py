6171c8251ff2e2a3354aa9ece2471e2f45db4b6f
Neža Vižintin <neza.vizintin@yahoo.com>
2023-09-21 00:18:27 +0200

    Update homework_2.py

--- homework_2.py
+++ homework_2.py
@@ -17,7 +17,7 @@ db.pretty_print("""
                 """)
 
 print("Track with MediaType Protected AAC audio file:")
-db.pretty_print("SELECT Track.Name FROM Track INNER JOIN MediaType ON Track.MediaTypeId=MediaType.MediaTypeId Where MediaType.Name = 'Protected AAC audio file'")
+db.pretty_print("SELECT COUNT(*) as ProtectedAACCount FROM Track INNER JOIN MediaType ON Track.MediaTypeId=MediaType.MediaTypeId Where MediaType.Name = 'Protected AAC audio file'")
 
 print("Artist with the most albums:")
 db.pretty_print("SELECT Artist.Name, COUNT(Artist.Name) as AlbumCount FROM Album INNER JOIN Artist ON Album.ArtistId=Artist.ArtistId GROUP BY Album.ArtistId  ORDER BY AlbumCount DESC LIMIT 1")
@@ -26,7 +26,7 @@ print("Genre with the most tracks:")
 db.pretty_print("SELECT Genre.Name, COUNT(Genre.Name) as GenreCount FROM Track INNER JOIN Genre ON Track.GenreId=Genre.GenreId GROUP BY Track.GenreId  ORDER BY GenreCount DESC LIMIT 1")
 
 print("Customer that has spent the most money:")
-db.pretty_print("SELECT MAX(Invoice.Total), Customer.FirstName, Customer.LastName FROM Invoice INNER JOIN Customer ON Invoice.CustomerId=Customer.CustomerId")
+db.pretty_print("SELECT SUM(Invoice.Total) as InvoiceSum, Customer.FirstName, Customer.LastName FROM Invoice INNER JOIN Customer ON Invoice.CustomerId=Customer.CustomerId GROUP BY Invoice.CustomerId ORDER BY InvoiceSum DESC LIMIT 1")
 
 print("Songs that were bought with each order:")
 db.pretty_print("SELECT Track.Name, Invoice.* FROM Track INNER JOIN InvoiceLine ON InvoiceLine.TrackId=Track.TrackId JOIN Invoice ON InvoiceLine.InvoiceId=Invoice.InvoiceId")
